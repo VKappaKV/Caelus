bb8571a7dfcb9216445e655eef6bc87cd9152c93
VKappaKV <jacopo851@gmail.com>
2025-01-24 21:12:03 +0100

    latest clients update

--- projects/Caelus/__test__/CaelusAdmin.test.ts
+++ projects/Caelus/__test__/CaelusAdmin.test.ts
@@ -27,24 +27,4 @@ describe('CaelusAdmin', () => {
 
     await appClient.create.createApplication({});
   });
-  /*
-
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
-  });
-
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
-  });
-
-  test('hello', async () => {
-    const diff = await appClient.hello({ name: 'world!' });
-    expect(diff.return?.valueOf()).toBe('Hello, world!');
-  }); */
 });
